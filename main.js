e810021f923196252dec281be462094e37fcd396
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-29 13:43:33 +0100

    DIsplay forecast weather and icons

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 import "./style.css";
 import API from "./config";
-import startWeatherApp from "./app";
 
-export const currentTemp = document.querySelector("#currentTemp");
-export const inputField = document.querySelector("#input-location");
-export const country = document.querySelector("#country");
-export const fetchBtn = document.querySelector(".fetch-btn");
-export const forecastCards = document.querySelector("#forecast-Card");
-export const weatherImg = document.querySelector("#weather-imgs")
-export const condition = document.querySelector("#condition")
-export const nav = document.querySelector(".nav")
-export const timeAndDate = document.querySelector("#time-date")
+const currentTemp = document.querySelector("#currentTemp");
+const inputField = document.querySelector("#input-location");
+const country = document.querySelector("#country");
+const fetchBtn = document.querySelector(".fetch-btn");
+const forecastCards = document.querySelector("#forecast-Card");
+const weatherImg = document.querySelector("#weather-imgs")
+const condition = document.querySelector("#condition")
+const nav = document.querySelector(".nav")
+const timeAndDate = document.querySelector("#time-date")
 
 
 const options = {
@@ -35,7 +34,7 @@ nav.appendChild(dateDisplay);
 updateClock();
 setInterval(updateClock, 1000);
 
-export const daysOfTheWeek = [
+const daysOfTheWeek = [
 	"Monday",
 	"Tuesday",
 	"Wednesday",
@@ -44,6 +43,94 @@ export const daysOfTheWeek = [
 	"Saturday",
 	"Sunday",
 ];
+
+
+const getWeatherData = async () => {
+	const cityName = inputField.value;
+	const url = "http://api.weatherapi.com/v1/forecast.json?key=" + API.key + "&q=" + cityName + "&days=7&aqi=no&alerts=no"
+
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
+
+
+
+const forecastDisplay = async (is_day, cityName) => {
+	console.log(API)
+
+	try {
+		const getData = await getWeatherData(cityName);
+		const forecastData = getData.forecast.forecastday;
+
+		for (let i = 0; i < forecastData.length; i++) {
+			const cards = document.createElement("div");
+			cards.classList.add("cards");
+
+			const dayData = forecastData[i];
+			const day = dayData.date
+			const temperature = dayData.day.avgtemp_c;
+			const tempText = dayData.day.condition.text;
+			const iconUrl = dayData.day.condition.icon;
+
+			const dayElement = document.createElement("p");
+			dayElement.textContent = day;
+
+			const temperatureElement = document.createElement("p");
+			temperatureElement.textContent = `${temperature} °C`;
+
+			const conditionElement = document.createElement("p");
+			conditionElement.textContent = tempText;
+
+			const iconImg = document.createElement("img");
+			iconImg.src = `http:${iconUrl}`;
+			iconImg.alt = "Weather Icon";
+
+			cards.appendChild(dayElement);
+			cards.appendChild(temperatureElement);
+			cards.appendChild(conditionElement);
+			cards.appendChild(iconImg);
+
+			forecastCards.appendChild(cards);
+
+		}
+	} catch (error) {
+		console.error("not working or getting data")
+	}
+
+}
+const startWeatherApp = async () => {
+	const cityName = inputField.value;
+	try {
+		const country = document.querySelector("#country");
+		const getData = await getWeatherData(cityName);
+		console.log("get all the data", getData);
+
+		if (getData.location) {
+			const { lat, lon } = getData.location;
+			const city = getData.location.name;
+			const Country = getData.location.country;
+			const temperature = getData.current.temp_c;
+			const tempText = getData.current.condition.text;
+			const iconUrl = getData.current.condition.icon;
+
+			country.innerHTML = `${city}, ${Country}`;
+			currentTemp.innerHTML = `${temperature} °C`
+			condition.innerHTML = `${tempText}`
+
+			const iconImg = document.createElement("img");
+			iconImg.src = `http:${iconUrl}`;
+			iconImg.alt = "Weather Icon"
+
+			weatherImg.innerHTML = ""
+			weatherImg.appendChild(iconImg);
+			forecastDisplay(null, cityName);
+		}
+	} catch (error) {
+		alert("Something is wrong, check error message");
+		console.error("Error fetching data", error);
+	}
+};
 
 inputField.addEventListener("keyup", function (event) {
 	if (event.key === "Enter") {
